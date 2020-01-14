b98b2a369f41eb646e78c56c3183f34e091d3be5
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 21:24:00 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
-print('hello')
-print('world')
-print('hello')
+def is_reverse(word1, word2):
+    if len(word1) != len(word2):
+       return False
+      
+    i = 0
+    j = len(word2)
+    
+    while j > 0:
+      if word1[i] != word2[j]:
+         return False
+        
+      i = i+1
+      j = j - 1
+    
+    return True
